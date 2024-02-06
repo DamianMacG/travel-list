ddb0030eceb1ff58a5276c8814ca3ec16c87f3e1
damiang <damiangildea12@gmail.com>
2024-02-06 11:22:40 +0000

    Travel list functionality completed

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,9 +1,24 @@
-function Stats() {
+function Stats({ items }) {
+    if (!items.length)
+      return (
+        <p className="stats">
+          <em>Start adding some items to your packing list 🚀</em>
+        </p>
+      );
+  
+    const numItems = items.length;
+    const numPacked = items.filter((item) => item.packed).length;
+    const percentage = Math.round((numPacked / numItems) * 100);
+  
     return (
-        <footer>
-           <em>You have X items on your list, and you already packed Y (X%)</em>
-        </footer>
-    )
-}
+      <footer className="stats">
+        <em>
+          {percentage === 100
+            ? "You got everything! Ready to go ✈️"
+            : ` 💼 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
+        </em>
+      </footer>
+    );
+  }
 
 export default Stats
